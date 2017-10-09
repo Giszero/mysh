e23c15813f215185af59b75eb1e33071e5807ca3
Giszero <32524678+Giszero@users.noreply.github.com>
2017-10-09 23:51:44 +0900

    Update utils.c

--- mysh-0-master/src/utils.c
+++ mysh-0-master/src/utils.c
@@ -1,7 +1,33 @@
 #include "utils.h"
+#include <stdio.h>
+#include <string.h>
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+
+#define MAX 255
+
+int num = 0;
+int num1 = 0;
+int i = 0;
+
+char str[MAX];
+char ch;
+
+printf("input");
+gets(str);
+
+for(i=0;i<strlen(str);i++)
+{
+
+ch = str[i];
+if(ch == ' ')
+num++
+}
+  
+num + 1 = num1;
+
+printf (argc == %d \n, num1);
+  
 }
