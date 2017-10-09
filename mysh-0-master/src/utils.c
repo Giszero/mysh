97c56925ebe8121fdd97b1116a05d8d2b718d14d
Giszero <32524678+Giszero@users.noreply.github.com>
2017-10-10 03:36:11 +0900

    Update utils.c

--- mysh-0-master/src/utils.c
+++ mysh-0-master/src/utils.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-int allocMemory(char ****ptr, int size)    // 반환값 없음, void 이중 포인터 매개변수 지정
+int allocMemory(char ***ptr, int size)    // 반환값 없음, void 이중 포인터 매개변수 지정
 {
     *ptr = malloc(size);    // void **ptr을 역참조하여 void *ptr에 메모리 할당
     return 1;
@@ -38,7 +38,7 @@ void mysh_parse_command(const char* command,
     temp_command[strlen(command)] = '\0';
 */
     //argv = malloc(1024); // allocate space
-    allocMemory(&argv, 1024);
+    allocMemory(argv, 1024);
   
     char *ptr = strtok(command, " ");
     while(ptr != NULL)
