6492cca93361d07b2d8cd0f63a7cb00e1b9fc733
Giszero <32524678+Giszero@users.noreply.github.com>
2017-10-10 02:41:59 +0900

    Update utils.c

--- mysh-0-master/src/utils.c
+++ mysh-0-master/src/utils.c
@@ -6,36 +6,38 @@ void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
   
-int num = 0;
-int num1 = 0;
-int i,j = 0;
+    int num = 0;
+    int num1 = 0;
+    int i,j = 0;
 
-char ch;
+    char ch;
 
-for(i=0;i<strlen(command);i++)
-{
-ch = command[i];
-if(ch == ' ')
-num++
-}
-num1 = num + 1;
+    for(i=0;i<strlen(command);i++)
+    {
+          ch = command[i];
+          if(ch == ' ')
+          num++
+     }
+    
+    num1 = num + 1;
   
- *argc = num1;  
+    *argc = num1;  
 
-printf ("argc == %d \n", *argc);
+    printf ("argc == %d \n", *argc);
   
-char ***command != strtok(command, " ");
+    char ***command != strtok(command, " ");
   
-while(command != NULL)
-{ for(j=0; j<*argc ; j++){
-
-  printf("&s\n",command);
-  strncpy(argv[j],command,strlen(command));
-  argv[j][strlen(command)] = '\0';
-  command = strtok(NULL," ");
+    while(command != NULL)
+    {
+       for(j=0; j<*argc ; j++)
+       {
+         printf("&s\n",command);
+         strncpy(argv[j],command,strlen(command));
+         argv[j][strlen(command)] = '\0';
+         command = strtok(NULL," ");
   
-}
+        }
   
-}
+    }
   
 }
