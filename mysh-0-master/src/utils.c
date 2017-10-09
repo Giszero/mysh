bb98e06c165576b434a05495338045440d33b304
Giszero <32524678+Giszero@users.noreply.github.com>
2017-10-10 03:44:56 +0900

    Update utils.c

--- mysh-0-master/src/utils.c
+++ mysh-0-master/src/utils.c
@@ -31,6 +31,24 @@ void mysh_parse_command(const char* command,
     strncpy(temp_command, command, strlen(command));
     temp_command[strlen(command)] = '\0';
 */
+  
+  
+    char **arrs; // declare arrs with pointer of char pointer.
+	arrs = (char**)calloc(MAXNUM, sizeof(char*)); // allocate 50*4bytes to arrs. it holds address of each string array.
+	int i;
+	for(i=0;i<MAXNUM;i++) {
+		arrs[i] = (char*)calloc(sizeof(char),13); // allocate memory of each string
+		strcpy(arrs[i],"TEST PROGRAM");
+	}
+
+	i = 0;
+	while(i<MAXNUM) { // print and free.
+		printf("%x : %s", (int)arrs[i], arrs[i]);
+		free(arrs[i]);
+	}
+	free(arrs[i]); // free arrs.
+  
+  /*
     *argv = (char **)calloc(64, sizeof(char*)); // allocate space
     for(i=0;i < 64;i++){
        *argv[i] = (char*)calloc(sizeof(char),16); // allocate memory of each string
@@ -51,5 +69,5 @@ void mysh_parse_command(const char* command,
         }
   
     }
-  
+  */
 }
