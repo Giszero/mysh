e31c8c676ca16621d7e250280f4e59847f9c588f
Giszero <32524678+Giszero@users.noreply.github.com>
2017-10-10 06:35:40 +0900

    Update commands.c

--- mysh-0-master/src/commands.c
+++ mysh-0-master/src/commands.c
@@ -23,13 +23,35 @@ int do_pwd(int argc, char** argv) {
 
 int validate_cd_argv(int argc, char** argv) {
   // TODO: Fill it!
+  if(argc != 2)
+  {
+    printf("cd must need argc = 2\n");
+    return 0;
+  }
+  if (argv[1]== ".")
+  {
+    return 0; 
+  }
+  if (!strncmp(argv[1], "..",3))
+  {
+     char buff[1024] = {0};
+  
+    getcwd(buff, 1024) //save to buff
+    if((strlen(buff) == 1) && (strcmp(buff, "/") == 0))
+    {
+      return 0;
+    }
+  }
+  
+  //Is it real?
+  
   return 1;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
  //argc(1) != error
   if(argc != 1){
-    printf("pwd must need argc=1");
+    printf("pwd must need argc=1\n");
     return 0;
   }
   return 1;
