f367970b4817361d310d173d846543d2aab2ef93
Giszero <32524678+Giszero@users.noreply.github.com>
2017-10-10 05:03:28 +0900

    Update utils.c

--- mysh-0-master/src/utils.c
+++ mysh-0-master/src/utils.c
@@ -10,24 +10,27 @@ void mysh_parse_command(const char* command, int *argc, char*** argv)
 	int i, j = 0;
 	char ch;
 
-	for (i = 0; i<strlen(command); i++) {
+	for (i = 0; i<strlen(command); i++)
+	{
 		ch = command[i];
-		if (ch == ' ') {
+		if (ch == ' ')
+		{
 			num++;
 		}
 	}
 	num1 = num + 1;
 	*argc = num1;
 	printf("argc == %d \n", *argc);
-
-
+	
+	// to avoid strtok warning
 	char* temp_command = (char *)malloc(sizeof(command));
 	strncpy(temp_command, command, strlen(command));
 	temp_command[strlen(command)] = '\0';
-	printf("%s\n", temp_command);
 
+	// dynamic allocator
 	*argv = (char **)calloc(*argc, sizeof(char*)); // allocate space
-	for (i = 0; i < *argc; i++) {
+	for (i = 0; i < *argc; i++)
+	{
 		(*argv)[i] = (char*)calloc(16, sizeof(char)); // allocate memory of each string
 	}
 
@@ -36,7 +39,6 @@ void mysh_parse_command(const char* command, int *argc, char*** argv)
 	{
 		for (j = 0; j<*argc; j++)
 		{
-			printf("=> %s\n", ptr);
 			strncpy((*argv)[j], ptr, strlen(ptr));
 			printf("argv=> %s\n", (*argv)[j]);
 			//argv[j][strlen(ptr)] = '\0';
